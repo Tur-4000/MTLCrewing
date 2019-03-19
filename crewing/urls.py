f555b78933e3d3ba348f34caf2b56d82bdbb2df6
Turbanov Valeriy <tur.4000@gmail.com>
2019-03-19 12:34:15 +0200

    seaman card

--- crewing/urls.py
+++ crewing/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.seamans_list, name='seamans_list'),
     path('seaman/', views.seamancard, name='seamancard'),
+    path('seaman/card/<int:seaman_id>/', views.seamancard, name='seamancard'),
     path('seaman/add/', views.seaman_add, name='seaman_add'),
+    path('seaman/edit/<int:seaman_id>/', views.seaman_edit, name='seaman_edit'),
 ]
--- crewing/views.py
+++ crewing/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 
 from .models import Seamans
 from .forms import SeamanForm
@@ -9,11 +9,13 @@ def seamans_list(request):
     return render(request, 'crewing/seamans_list.html', {'seamans': seamans})
 
 
-def seamancard(request):
-    return render(request, 'crewing/seamancard.html')
+def seamancard(request, seaman_id):
+    seaman = get_object_or_404(Seamans, id=seaman_id)
+    return render(request, 'crewing/seamancard.html', {'seaman': seaman})
 
 
 def seaman_add(request):
+    title = 'Добавить моряка'
     form = SeamanForm()
     if request.method == 'POST':
         form = SeamanForm(request.POST)
@@ -21,6 +23,25 @@ def seaman_add(request):
             form.save()
             return redirect('seamans_list')
         else:
-            return render(request, 'crewing/seaman.html', {'form': form, })
+            return render(request, 'crewing/seaman.html',
+                          {'form': form, 'title': title})
     else:
-        return render(request, 'crewing/seaman.html', {'form': form})
+        return render(request, 'crewing/seaman.html',
+                      {'form': form, 'title': title})
+
+
+def seaman_edit(request, seaman_id):
+    title = 'Редактировать моряка'
+    seaman = get_object_or_404(Seamans, id=seaman_id)
+    if request.method == 'POST':
+        form = SeamanForm(request.POST, instance=seaman)
+        if form.is_valid():
+            form.save()
+        else:
+            return render(request, 'crewing/seaman.html',
+                          {'form': form, 'seaman': seaman, 'title': title})
+        return redirect('seamans_list')
+    else:
+        form = SeamanForm(instance=seaman)
+        return render(request, 'crewing/seaman.html',
+                      {'form': form, 'seaman': seaman, 'title': title})
