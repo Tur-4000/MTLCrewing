0dce05a0f5023f37f1778e187739587ea4518782
Turbanov Valeriy <tur.4000@gmail.com>
2019-03-21 10:57:46 +0200

    добавление отзывов

--- crewing/urls.py
+++ crewing/urls.py
@@ -8,10 +8,13 @@ urlpatterns = [
     path('seaman/card/<int:seaman_id>/', views.seamancard, name='seamancard'),
     path('seaman/add/', views.seaman_add, name='seaman_add'),
     path('seaman/edit/<int:seaman_id>/', views.seaman_edit, name='seaman_edit'),
+    path('seaman/<int:seaman_id>/opinion/add/', views.opinion_add, name='opinion_add'),
+    path('seaman/<int:seaman_id>/contract/add/', views.contract_add, name='contract_add'),
     path('rank/list/', views.rank_list, name='rank_list'),
     path('rank/add/', views.rank_add, name='rank_add'),
     path('rank/edit/<int:rank_id>/', views.rank_edit, name='rank_edit'),
     path('vessel/list/', views.vessels_list, name='vessels_list'),
     path('vessel/add/', views.vessel_add, name='vessel_add'),
-    path('vessel/edit/<int:vessel_id>/', views.vessel_edit, name='vessel_edit')
+    path('vessel/edit/<int:vessel_id>/', views.vessel_edit, name='vessel_edit'),
+
 ]
