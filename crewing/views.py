87a9013b1aef892f9a05747d53abb1557255c44c
Turbanov <tur.4000@gmail.com>
2019-03-25 22:23:13 +0200

    привёл передачу параметров к единому виду

--- crewing/views.py
+++ crewing/views.py
@@ -9,99 +9,97 @@ from .utils import last_rank
 
 def seamans_list(request):
     seamans = Seamans.objects.all()
-    return render(request, 'crewing/seamans_list.html', {'seamans': seamans})
+    context = {'seamans': seamans}
+    return render(request, 'crewing/seamans_list.html', context)
 
 
 def seamancard(request, seaman_id):
     seaman = get_object_or_404(Seamans, id=seaman_id)
     contracts = Contracts.objects.filter(seaman=seaman_id).all()
     opinions = Opinions.objects.filter(seaman=seaman_id).all()
-    return render(request, 'crewing/seamancard.html',
-                  {'seaman': seaman, 'contracts': contracts, 'opinions': opinions})
+    context = {'seaman': seaman, 'contracts': contracts, 'opinions': opinions}
+    return render(request, 'crewing/seamancard.html', context)
 
 
 def seaman_add(request):
     title = 'Добавить моряка'
+
     if request.method == 'POST':
         form = SeamanForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
             return redirect('seamans_list')
-        else:
-            return render(request, 'crewing/seaman.html',
-                          {'form': form, 'title': title})
     else:
         form = SeamanForm()
-        return render(request, 'crewing/seaman.html',
-                      {'form': form, 'title': title})
+
+    context = {'form': form, 'title': title}
+    return render(request, 'crewing/seaman.html', context)
 
 
 def seaman_edit(request, seaman_id):
     title = 'Редактировать моряка'
     seaman = get_object_or_404(Seamans, id=seaman_id)
+
     if request.method == 'POST':
         form = SeamanForm(request.POST, request.FILES, instance=seaman)
         if form.is_valid():
             form.save()
-        else:
-            return render(request, 'crewing/seaman.html',
-                          {'form': form, 'seaman': seaman, 'title': title})
-        return redirect('seamans_list')
+            return redirect('seamans_list')
     else:
         form = SeamanForm(instance=seaman)
-        return render(request, 'crewing/seaman.html',
-                      {'form': form, 'seaman': seaman, 'title': title})
+
+    context = {'form': form, 'seaman': seaman, 'title': title}
+    return render(request, 'crewing/seaman.html', context)
 
 
 def rank_list(request):
     title = 'Справочник должностей'
     ranks = Ranks.objects.all()
-    return render(request, 'crewing/rank_list.html',
-                  {'ranks': ranks, 'title': title})
+    context = {'ranks': ranks, 'title': title}
+    return render(request, 'crewing/rank_list.html', context)
 
 
 def rank_add(request):
     title = 'Добавить должность'
-    form = RankForm()
+
     if request.method == 'POST':
         form = RankForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect('rank_list')
-        else:
-            return render(request, 'crewing/rank.html',
-                   {'title': title, 'form': form})
     else:
-        return render(request, 'crewing/rank.html',
-                      {'title': title, 'form': form})
+        form = RankForm()
+
+    context = {'title': title, 'form': form}
+    return render(request, 'crewing/rank.html', context)
 
 
 def rank_edit(request, rank_id):
     title = 'Редактировать должность'
     rank = get_object_or_404(Ranks, id=rank_id)
+
     if request.method == 'POST':
         form = RankForm(request.POST, instance=rank)
         if form.is_valid():
             form.save()
             return redirect('rank_list')
-        else:
-            return render(request, 'crewing/rank.html',
-                   {'title': title, 'form': form, 'rank': rank})
     else:
         form = RankForm(instance=rank)
-        return render(request, 'crewing/rank.html',
-                      {'title': title, 'form': form, 'rank': rank})
+    context = {'title': title, 'form': form, 'rank': rank}
+
+    return render(request, 'crewing/rank.html', context)
 
 
 def vessels_list(request):
     title = 'Справочник судов'
     vessels = Vessels.objects.all()
-    return render(request, 'crewing/vessels_list.html',
-                  {'title': title, 'vessels': vessels})
+    context = {'title': title, 'vessels': vessels}
+    return render(request, 'crewing/vessels_list.html', context)
 
 
 def vessel_add(request):
     title = 'Добавить судно'
+
     if request.method == 'POST':
         form = VesselForm(request.POST)
         if form.is_valid():
@@ -112,31 +110,32 @@ def vessel_add(request):
                           {'title': title, 'form': form})
     else:
         form = VesselForm()
-        return render(request, 'crewing/vessel.html',
-                      {'title': title, 'form': form})
+
+    context = {'title': title, 'form': form}
+    return render(request, 'crewing/vessel.html', context)
 
 
 def vessel_edit(request, vessel_id):
     title = 'Редактировать судно'
     vessel = get_object_or_404(Vessels, id=vessel_id)
+
     if request.method == 'POST':
         form = VesselForm(request.POST, instance=vessel)
         if form.is_valid():
             form.save()
             return redirect('vessels_list')
-        else:
-            return render(request, 'crewing/vessel.html',
-                          {'title': title, 'form': form, 'vessel': vessel})
     else:
         form = VesselForm(instance=vessel)
-        return render(request, 'crewing/vessel.html',
-                      {'title': title, 'form': form, 'vessel': vessel})
+
+    context = {'title': title, 'form': form, 'vessel': vessel}
+    return render(request, 'crewing/vessel.html', context)
 
 
 def opinion_add(request, seaman_id):
     title = 'Добавить отзыв'
     seaman = get_object_or_404(Seamans, id=seaman_id)
     qs = Contracts.objects.filter(seaman=seaman_id).all()
+
     if request.method == 'POST':
         form = OpinionForm(request.POST, contracts=qs)
         if form.is_valid():
@@ -146,6 +145,7 @@ def opinion_add(request, seaman_id):
             return redirect('seamancard', seaman_id)
     else:
         form = OpinionForm(contracts=qs)
+
     context = {'title': title, 'form': form, 'seaman': seaman}
     return render(request, 'crewing/opinion.html', context)
 
@@ -155,25 +155,24 @@ def opinion_edit(request, seaman_id, opinion_id):
     seaman = get_object_or_404(Opinions, id=seaman_id)
     opinion = get_object_or_404(Opinions, id=opinion_id)
     qs = Contracts.objects.filter(seaman=seaman_id).all()
+
     if request.method == 'POST':
         form = OpinionForm(request.POST, instance=opinion, contracts=qs)
         if form.is_valid():
             form.save()
             return redirect('seamancard', seaman.id)
-        else:
-            render(request, 'crewing/opinion.html',
-                   {'title': title, 'form': form,
-                    'opinion': opinion, 'seaman': seaman})
     else:
         form = OpinionForm(instance=opinion, contracts=qs)
-        return render(request, 'crewing/opinion.html',
-                      {'title': title, 'form': form,
-                       'opinion': opinion, 'seaman': seaman})
+
+    context = {'title': title, 'form': form,
+               'opinion': opinion, 'seaman': seaman}
+    return render(request, 'crewing/opinion.html', context)
 
 
 def contract_add(request, seaman_id):
     title = 'Добавить контракт'
     seaman = get_object_or_404(Seamans, id=seaman_id)
+
     if request.method == 'POST':
         form = ContractForm(request.POST)
         if form.is_valid():
@@ -183,19 +182,18 @@ def contract_add(request, seaman_id):
             seaman.last_rank = last_rank(seaman_id)
             seaman.save()
             return redirect('seamancard', seaman_id)
-        else:
-            return render(request, 'crewing/contract.html',
-                  {'title': title, 'form': form, 'seaman': seaman})
     else:
         form = ContractForm()
-        return render(request, 'crewing/contract.html',
-                      {'title': title, 'form': form, 'seaman': seaman})
+
+    context = {'title': title, 'form': form, 'seaman': seaman}
+    return render(request, 'crewing/contract.html', context)
 
 
 def contract_edit(request, seaman_id, contract_id):
     title = 'Редактировать контракт'
     contract = get_object_or_404(Contracts, id=contract_id)
     seaman = get_object_or_404(Seamans, id=seaman_id)
+
     if request.method == 'POST':
         form = ContractForm(request.POST, instance=contract)
         if form.is_valid():
@@ -207,6 +205,7 @@ def contract_edit(request, seaman_id, contract_id):
             return redirect('seamancard', seaman_id)
     else:
         form = ContractForm(instance=contract)
+
     context = {'title': title, 'form': form, 'seaman': seaman}
     return render(request, 'crewing/contract.html', context)
 
@@ -214,21 +213,20 @@ def contract_edit(request, seaman_id, contract_id):
 def seamans_questions_list(request):
     title = 'Вопросы рейтинга 360 (моряки)'
     questions = Seaman360Question.objects.all()
-    return render(request, 'crewing/rating_seaman.html',
-                  {'title': title, 'questions': questions})
+    context = {'title': title, 'questions': questions}
+    return render(request, 'crewing/rating_seaman.html', context)
 
 
 def seamans_questions_add(request):
     title = 'Добавить вопрос рейтинга 360 (моряки)'
+
     if request.method == 'POST':
         form = Seaman360QuestionForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect('seamans_questions_list')
-        else:
-            return render(request, 'crewing/question.html',
-                          {'title': title, 'form': form})
     else:
         form = Seaman360QuestionForm()
-        return render(request, 'crewing/question.html',
-                      {'title': title, 'form': form})
+
+    context = {'title': title, 'form': form}
+    return render(request, 'crewing/question.html', context)
